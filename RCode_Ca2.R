9f82b322129efb8b8948848a0140c643919186d1
Stephen Kane <stephenkane@Stephens-MacBook-Pro-2.local>
2020-03-03 16:05:00 +0000

    t+t

--- RCode_Ca2.R
+++ RCode_Ca2.R
@@ -1,1 +1,19 @@
 #test 
+#Load in the data
+getwd()
+setwd("/Users/stephenkane/downloads")
+Road <- read.csv(file="uk-road-safety-accidents-and-vehicles/Accident_Information.csv", header = TRUE, sep =",")
+
+#Sample Road Safety
+set.seed(404)
+road.index <- sample(1:nrow(Road),ceiling(0.05*nrow(Road)))
+road.train <- Road[road.index,]
+road.test <- Road[-road.index,]
+
+#Remove raw dataset
+rm(Road)
+
+#export sample data
+write.csv(file = "Road_Train.csv", road.train, row.names = FALSE)
+
+Vehicle <- read.csv(file="uk-road-safety-accidents-and-vehicles/Vehicle_Information.csv", header = TRUE, sep =",")
